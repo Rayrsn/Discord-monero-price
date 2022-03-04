fd08b3cf30986e6c31d9e399201712a4a79a16b1
numberx <shekooha696@gmail.com>
2022-03-04 14:47:55 +0330

    Customized script to my own needs

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
-import json
-from dhooks import Webhook
 import requests
 import time
-hook = Webhook('WEBHOOK HERE')
-url = 'https://api.coingecko.com/api/v3/simple/price?ids=Monero&vs_currencies=usd'
+from datetime import datetime
+from dhooks import Webhook
+from datetime import datetime
+from kavenegar import KavenegarAPI
+
+
+#CONFIG
+IDS = 'monero'
+CURRENCY = 'usd'
+
+# OPTIONAL
+
+# if send by discord web hook
+WEBHOOK = ''
+
+
+# get it from kavenegar.com
+# if send by sms
+PHONE_NUMBER = ''
+
+
+def get_price():
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={IDS}&vs_currencies={CURRENCY}"
+    r = requests.get(url)
+
+    price = r.json()[IDS][CURRENCY]
+    time = datetime.now()
+    return f"{IDS}   is    {price}     {CURRENCY}'s    ---         {time}" 
+
+
+def send_by_sms(kavenegar_api_key):
+    api = KavenegarAPI(kavenegar_api_key)
+    price = get_price()
+    payload = {
+        "sender": '8585858585858585',
+        "receptor" : PHONE_NUMBER,
+        'message': price,
+    }
+    res = api.sms_send(params=payload)
+    print (res)
+
+def send_to_discord():
+    hook = Webhook(WEBHOOK)
+    price = get_price()
+    hook.send(price)
+
 while True:
     try:
-        r = requests.get(url)
-        price = json.dumps(r.json()['monero']['usd'])
-        print(price)
-        hook.send(price)
+        send_by_sms()
         time.sleep(3600)
     except Exception as e:
         print(e)
